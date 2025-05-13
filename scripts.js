d32f6d4c86eac0375ee714ae16f80439ad9294ba
Estevao Nogueira <estevaofilipenogueira@gmail.com>
2025-05-13 19:04:07 -0300

    Update: novo cliente

---
+++ scripts.js
@@ -0,0 +1,14 @@
+class Cliente {
+  nome;
+  cpf;
+};
+
+class ContaCorrente {
+  agencia;
+  saldo;
+};
+
+const cliente1 = new Cliente();
+
+cliente1.nome = "Estêvão";
+cliente1.cpf = 60083880092;
