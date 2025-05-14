c725a255e313c69ec40175f1bb0887742ce71341
Estevao Nogueira <estevaofilipenogueira@gmail.com>
2025-05-14 20:29:21 -0300

    Fds

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
-class Cliente {
-  nome;
-  cpf;
-};
-
-class ContaCorrente {
-  agencia;
-  saldo;
-};
+import { ContaCorrente } from "./ContaCorrente.js";
+import { Cliente } from "./Cliente.js";
 
 const cliente1 = new Cliente();
 
 cliente1.nome = "Estêvão";
 cliente1.cpf = 60083880092;
+
+const contaCliente1 = new ContaCorrente()
+
+contaCliente1.saldo = 100;
+contaCliente1.agencia = 1001;
+contaCliente1.cliente = cliente1;
+
+console.log(contaCliente1)
